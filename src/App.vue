087c7d7b016210e3efbdb016db2bee40d34e7c73
Jens Vanhulst <vanhulstjens@gmail.com>
2022-09-22 19:35:50 +0200

    add pizza working

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,38 @@
     <h1 class="title">Solita Pizza Maker</h1>
     <div class="box">
       <div class="left-container">
-        <div class="option">{{ topping }}?</div>
+        <div class="option">{{ pizza.topping }} ?</div>
         <div class="option-container">
-          <div v-if="showSauceCan" class="sauce-can">
+          <div v-if="pizza.showSauceCan" class="sauce-can">
             <div class="top-rim"></div>
             <div class="bottom-rim"></div>
             <div class="red-label"></div>
             <div class="badge"></div>
           </div>
-          <div v-if="showShreddedCheese" class="shredded-cheese-wedge">
+          <div v-if="pizza.showShreddedCheese" class="shredded-cheese-wedge">
             <div class="wedge-circle-1"></div>
             <div class="wedge-circle-2"></div>
             <div class="wedge-circle-3"></div>
           </div>
-          <div v-if="showMozzarellaCheese" class="moz-wedge"></div>
-          <div v-if="showPepperoniTopping" class="pep-option"></div>
+          <div v-if="pizza.showMozzarellaCheese" class="moz-wedge"></div>
+          <div v-if="pizza.showPepperoniTopping" class="pep-option"></div>
         </div>
         <div v-if="buttonsOn" @click="processInput(1)" class="yes-button">YES</div>
-        <div v-if="buttonsOn" @click="processInput(2)" class="no-button">NO</div>
+        <div v-if="buttonsOn && !resetButton" @click="processInput(2)" class="no-button">NO</div>
       </div>
       <div class="right-container">
         <div class="option">
           <span id="white">{{ totalCost }}</span>
         </div>
         <div :class="pizzaClass">
-          <div v-if="showSauce" class="sauce"></div>
-          <div v-if="showWedgeCheese" class="wedge-cheese">
+          <div v-if="pizza.showSauce" class="sauce"></div>
+          <div v-if="pizza.showWedgeCheese" class="wedge-cheese">
             <div class="cheese-1"></div>
             <div class="cheese-2"></div>
             <div class="cheese-3"></div>
           </div>
-          <div v-if="showStringCheese" class="string-cheese"></div>
-          <div v-if="showPepperoni" class="pepperoni">
+          <div v-if="pizza.showStringCheese" class="string-cheese"></div>
+          <div v-if="pizza.showPepperoni" class="pepperoni">
             <div class="pepperoni-1"></div>
             <div class="pepperoni-2"></div>
             <div class="pepperoni-3"></div>
@@ -46,71 +46,97 @@
 </template>
 
 <script setup>
+import axios from "axios";
 import { ref, reactive } from "vue";
 
 //left container
-const topping = ref("Sauce");
-
-const buttonsOn = ref(true);
-const showSauceCan = ref(true);
-const showShreddedCheese = ref(false);
-const showMozzarellaCheese = ref(false);
-const showPepperoniTopping = ref(false);
-
-//right container
-let pizzaClass = "pizza";
-const showSauce = ref(false);
-const showWedgeCheese = ref(false);
-const showStringCheese = ref(false);
-const showPepperoni = ref(false);
-const currentPrice = ref(0);
+let stage = 1;
+let choices = [];
+let pizzaClass = ref("pizza");
 const totalCost = ref("$0");
-
+const buttonsOn = ref(true);
+const resetButton = ref(false);
+const currentPrice = ref(0);
 const prices = reactive([2, 2, 3, 3, 3]);
 
-//count which option we are on
-let stage = 1;
+let pizza = reactive({
+  topping: "Sauce",
+  showSauce: false,
+  showWedgeCheese: false,
+  showStringCheese: false,
+  showPepperoni: false,
+  showSauceCan: true,
+  showShreddedCheese: false,
+  showMozzarellaCheese: false,
+  showPepperoniTopping: false,
+});
 
 function processInput(selection) {
   if (selection === 1) {
     //yes button clicked and do stuff accordingly
     if (stage === 1) {
-      showSauce.value = true;
+      pizza.showSauce = true;
+      choices.push("Tomato Sauce");
     } else if (stage === 2) {
-      showStringCheese.value = true;
+      pizza.showStringCheese = true;
+      choices.push("Shredded Cheese");
     } else if (stage === 3) {
-      showWedgeCheese.value = true;
+      pizza.showWedgeCheese = true;
+      choices.push("Mozzarella Cheese");
     } else if (stage === 4) {
-      showPepperoni.value = true;
+      pizza.showPepperoni = true;
+      choices.push("Pepperoni");
     }
 
     if (stage !== 5) {
       currentPrice.value += prices[stage - 1];
-      totalCost.value = "$" + currentPrice;
+      totalCost.value = "$" + currentPrice.value;
     }
   } else if (selection === 2) {
     //do nothing
   }
+
   if (stage === 1) {
-    showSauceCan.value = false;
-    showShreddedCheese.value = true;
-    topping.value = "Shredded Cheese";
+    pizza.showSauceCan = false;
+    pizza.showShreddedCheese = true;
+    pizza.topping = "Shredded Cheese";
   } else if (stage === 2) {
-    showShreddedCheese.value = false;
-    showMozzarellaCheese.value = true;
-    topping.value = "Mozzarella Cheese";
+    pizza.showShreddedCheese = false;
+    pizza.showMozzarellaCheese = true;
+    pizza.topping = "Mozzarella Cheese";
   } else if (stage === 3) {
-    showMozzarellaCheese.value = false;
-    showPepperoniTopping.value = true;
-    topping.value = "Pepperoni";
+    pizza.showMozzarellaCheese = false;
+    pizza.showPepperoniTopping = true;
+    pizza.topping = "Pepperoni";
   } else if (stage === 4) {
-    showPepperoniTopping.value = false;
-    topping.value = "Order";
-    pizzaClass = "spin-pizza";
+    pizza.showPepperoniTopping = false;
+    pizza.topping = "Order";
+    resetButton.value = true;
+    pizzaClass.value = "spin-pizza";
   } else if (stage === 5) {
-    buttonsOn.value = false;
-    topping.value = "Out of Order";
+    resetOrder();
   }
   stage++;
+}
+
+function resetOrder() {
+  alert("Pizza is ordered !");
+  window.location.reload();
+}
+
+function finishPurchase() {
+  axios.post(
+    "https://apim-kobe-test.azure-api.net/slack-notification",
+    {
+      text: "🍕 A new order for Pizza has arrived !",
+      choices,
+      price: totalCost.value,
+    },
+    {
+      headers: {
+        "Ocp-Apim-Subscription-Key": "eb10e500732e47cb88477af5ba1a85e8",
+      },
+    }
+  );
 }
 </script>
